b1cc38bc321b482dd4ff87fafc1e937d99c99d44
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2021-07-11 21:35:00 +0200

    html: Put CSS rules for navigation into a separate file

--- code/html/navigation.lisp
+++ code/html/navigation.lisp
@@ -5,13 +5,13 @@
 (defclass navigation-sidebar (transform:builder-mixin
                               output-directory-mixin
                               static-files-mixin)
-  ((%static-files :initform (list #+no (load-time-value
-                                        (a:read-file-into-string
-                                         ))
-                                  (cons #1=#P"navigation.js"
-                                        (merge-pathnames
-                                         #1# #.(or *compile-file-pathname*
-                                                   *load-pathname*)))))
+  ((%static-files :initform (let ((base #.(or *compile-file-pathname*
+                                              *load-pathname*)))
+                              (list #+no (load-time-value
+                                          (a:read-file-into-string
+                                           ))
+                                    (cons #1=#P"navigation.js"  (merge-pathnames #1# base))
+                                    (cons #2=#P"navigation.css" (merge-pathnames #2# base)))))
    ;; State
    (%index-files  :accessor index-files
                   :initform nil)
