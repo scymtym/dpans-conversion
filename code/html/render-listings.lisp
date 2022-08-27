a77feb76215fb4bfd494e1a7e60805e10bfa3615
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2022-08-27 12:56:06 +0200

    html: Fix rendering of listings and monospace style

--- code/html/render-listings.lisp
+++ code/html/render-listings.lisp
@@ -4,7 +4,11 @@
   (let ((class (format nil "~{syntax-~(~A~)~^ ~}" (a:ensure-list classes)))) ; TODO should always be a list
     (span class #'recurse)))
 
-(define-render (:listing)
-  (cxml:with-element "pre"
-    (cxml:with-element "code"
-      (recurse))))
+(define-render (:listing (in-line? nil))
+  (flet ((do-code ()
+           (cxml:with-element "code"
+             (recurse))))
+    (if in-line?
+        (do-code)
+        (cxml:with-element "pre"
+          (do-code)))))
